aac4dfc3c68791f726f3da1d4847fa8c2a93c7fa
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-07-21 13:06:38 +0200

    Los tipos de numeros

--- JavaScript/Curso de Fundamentos de JavaScript/4º_Números_y_Operaciones_Matemáticas/clase-number.js
+++ JavaScript/Curso de Fundamentos de JavaScript/4º_Números_y_Operaciones_Matemáticas/clase-number.js
@@ -1,4 +1,11 @@
 // 1. Tipo Entero y Decimal
 const entero = 42; // Entero
 const decimal = 3.14; // Decimal
-console.log(typeof)
+console.log(typeof entero, typeof decimal); // "number"
+
+// 2. Notación Científica
+const cientifico = 5e3; // 5 * 10^3
+
+// 3. Infinitos y NaN(Not a Number)
+const infinito = Infinity;
+const noEsUNNumero = NaN;
