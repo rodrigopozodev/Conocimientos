103d5069250fd0e86e97c211446a50604946559e
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-07-21 12:46:13 +0200

    Strings

---
+++ JavaScript/Curso de Fundamentos de JavaScript/3º_Strings/strings.js
@@ -0,0 +1,17 @@
+// Tipo de dato: string
+
+let string1 = 'Hola, mundo';
+let string2 = "JavaScript es genial";
+let string3 = `${string1} feliz :)`;
+let string4 =  string1 + " " + string2;
+
+// console.log(string1);
+// console.log(string2);
+// console.log(string3);
+// console.log(string4);
+
+let frase = "JavaScript es un lenguaje de programación versátil";
+console.log(frase.length); // Longitud de la cadena
+console.log(frase.toLowerCase()); // Convertir a minúsculas
+console.log(frase.toUpperCase()); // Convertir a mayúsculas
+console.log(frase.substring(0, 10)); // Subcadena (desde el índice 0 hasta el 10)
