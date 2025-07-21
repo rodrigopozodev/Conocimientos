1b92c8e68e000278589053cf2de038f005a594a6
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-07-21 12:02:57 +0200

    Tipos de datos:
    Primitivos y complejos

---
+++ JavaScript/Curso de Fundamentos de JavaScript/2º_Tipos_de_datos/tipos-de-datos.js
@@ -0,0 +1,36 @@
+// PRIMITIVOS
+
+// String
+let nombre = "Rodrigo";
+
+// Number
+let edad = 22;
+
+// Boolean
+let esMayorDeEdad = true;
+
+// Null
+let noHayValor = null;
+
+// Undefined
+let noDefinido = undefined;
+
+// Symbol
+let simboloUnico = Symbol("único");
+
+// Bigint
+let numeroGrande = 2n;
+
+// COMPLEJOS
+
+// Object
+let carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+}
+
+// array (listas)
+let frutas = ["manzana", "platano", "naranja"];
+
+// function
+function saludar () {}
