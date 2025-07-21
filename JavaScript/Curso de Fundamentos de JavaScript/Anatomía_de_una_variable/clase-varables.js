3c0bc458bac22401681b4ffc563abc208e979513
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-07-21 11:23:46 +0200

    Anatomia de variables

---
+++ JavaScript/Curso de Fundamentos de JavaScript/Anatomía_de_una_variable/clase-varables.js
@@ -0,0 +1,11 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// Lo NO recomendable
+let c = 'Woody'
+let cda = 'woody'
+let pcAndy = 'Woody'
+
+// Lo recomendable es que el nombre sea lo mas descriptivo posible
+let primerTrasteoDeAndy = 'Woody'
+
