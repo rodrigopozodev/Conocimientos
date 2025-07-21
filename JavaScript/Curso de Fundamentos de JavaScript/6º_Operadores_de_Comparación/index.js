79b711759ca63c9794da08a9829c3dd08b957653
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-07-21 20:13:59 +0200

    Operadores de comparación

---
+++ JavaScript/Curso de Fundamentos de JavaScript/6º_Operadores_de_Comparación/index.js
@@ -0,0 +1,17 @@
+/*
+
+Operadores de comparación
+
+*/
+
+const a = 10;
+const b = 20;
+const c = "10";
+
+a == b; // false
+a === c; // false
+a != b; // true
+a !== c; // true
+a > b; // false
+a <= b; // true
+a > c; // false
