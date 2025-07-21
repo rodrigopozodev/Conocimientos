8aa11594f0c1f0c57e3350b0bb30dd46018929d7
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-07-21 20:53:43 +0200

    Estructuras Condicionales If, else y else if

---
+++ JavaScript/Curso de Fundamentos de JavaScript/8º_Estructuras_Condicionales/index.js
@@ -0,0 +1,19 @@
+let nombre = "Diego";
+
+if (nombre === "Diego") {
+    console.log("¡Hola Diego!");
+}
+
+if (nombre === "Nico") {
+    console.log("¡Hola Nico!");
+} else {
+    console.log("Nombre no encontrado");
+}
+
+if (nombre === "Nico") {
+    console.log("¡Hola Nico!");
+} else if (nombre === "Diego") {
+    console.log("¡Hola Diego!");
+} else {
+    console.log("Nombre no encontrado");
+}
